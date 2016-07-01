5ce40befd9d06abccaf6482ba3a3ddc9f6922941
Plínio Naves <pliniopjn@hotmail.com>
2016-07-01 15:40:46 -0300

    change socket io

--- app/providers/socket-io/socket-io.ts
+++ app/providers/socket-io/socket-io.ts
@@ -7,7 +7,7 @@ export class SocketIo {
   private socket:any;
 
   constructor() {
-    this.socket = io.connect('https://dynamic-app-test-plinionaves.c9users.io:8080');
+    this.socket = io.connect('https://node-chat-server-plinionaves.c9users.io:8080');
   }
 
   getSocket() {
